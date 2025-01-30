26c65da6663cf9aa51ce0d9eccdab32abaf683c6
TrevorB20 <146786007+TrevorB20@users.noreply.github.com>
2025-01-30 18:40:29 -0500

    Update Main.tf

--- Terraform/Main.tf
+++ Terraform/Main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 #Creating EC2 Instance
 
 resource "aws_instance" "Myweb" {
-  ami           = "ami-0c614dee691cbbf37"
+  ami           = "Provide your own AMI"
   instance_type = "t2.micro"
 
   tags = {
