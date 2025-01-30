484dd1f737adfa78d2e2f279b8208567a6ec498b
TrevorB20 <trevorboodram1289@gmail.com>
2025-01-30 18:12:03 -0500

    Creating AWS EC2

---
+++ Terraform/Main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#Creating EC2 Instance
+
+resource "aws_instance" "Myweb" {
+  ami           = "ami-0c614dee691cbbf37"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "EC2 Terraform"
+  }
+}
